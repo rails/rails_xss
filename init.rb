53b40182fd54979b2339f34dd3c83ae87259317b
Phil Cohen <phil@binarytrees.net>
2010-10-14 22:27:47 -0700

    workaround ruby string compare for Rails.version

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 unless $gems_rake_task
-  if Rails.version <= "2.3.7"
-    $stderr.puts "rails_xss requires Rails 2.3.8 or later. Please upgrade to enable automatic HTML safety."
-  else
+  major, minor, micro = Rails.version.split(".").map(&:to_i)
+  if major == 2 && minor == 3 && micro >= 8
     require 'rails_xss'
+  else
+    $stderr.puts "rails_xss requires Rails 2.3.8 or later. Please upgrade to enable automatic HTML safety."
   end
 end
