844ac2068daede2b00c59c6dc4d6db257bc40a44
grosser <grosser.michael@gmail.com>
2012-09-11 09:03:15 -0700

    make sub! etc work like they do in rails 3 by copying the implementation

--- lib/rails_xss/string_ext.rb
+++ lib/rails_xss/string_ext.rb
@@ -1,26 +1,110 @@
 require 'active_support/deprecation'
 
-ActiveSupport::SafeBuffer.class_eval do
-  def concat(value)
-    if value.html_safe?
-      super(value)
-    else
-      super(ERB::Util.h(value))
-    end
-  end
-  alias << concat
-  UNSAFE_STRING_METHODS = ["capitalize", "chomp", "chop", "delete", "downcase", "gsub", "lstrip", "next", "reverse", "rstrip", "slice", "squeeze", "strip", "sub", "succ", "swapcase", "tr", "tr_s", "upcase"].freeze
 
-  for unsafe_method in UNSAFE_STRING_METHODS
-    class_eval <<-EOT, __FILE__, __LINE__ + 1
-      def #{unsafe_method}(*args)
-        super.to_str
+module ActiveSupport #:nodoc:
+  class SafeBuffer < String
+    UNSAFE_STRING_METHODS = %w(
+      capitalize chomp chop delete downcase gsub lstrip next reverse rstrip
+      slice squeeze strip sub succ swapcase tr tr_s upcase prepend
+    )
+
+    alias_method :original_concat, :concat
+    private :original_concat
+
+    class SafeConcatError < StandardError
+      def initialize
+        super 'Could not concatenate to the buffer because it is not html safe.'
       end
-  
-      def #{unsafe_method}!(*args)
-        raise TypeError, "Cannot modify SafeBuffer in place"
+    end
+
+    def [](*args)
+      if args.size < 2
+        super
+      else
+        if html_safe?
+          new_safe_buffer = super
+          new_safe_buffer.instance_eval { @html_safe = true }
+          new_safe_buffer
+        else
+          to_str[*args]
+        end
       end
-    EOT
+    end
+
+    def safe_concat(value)
+      raise SafeConcatError unless html_safe?
+      original_concat(value)
+    end
+
+    def initialize(*)
+      @html_safe = true
+      super
+    end
+
+    def initialize_copy(other)
+      super
+      @html_safe = other.html_safe?
+    end
+
+    def clone_empty
+      self[0, 0]
+    end
+
+    def concat(value)
+      if !html_safe? || value.html_safe?
+        super(value)
+      else
+        super(ERB::Util.h(value))
+      end
+    end
+    alias << concat
+
+    def +(other)
+      dup.concat(other)
+    end
+
+    def %(args)
+      args = Array(args).map do |arg|
+        if !html_safe? || arg.html_safe?
+          arg
+        else
+          ERB::Util.h(arg)
+        end
+      end
+
+      self.class.new(super(args))
+    end
+
+    def html_safe?
+      defined?(@html_safe) && @html_safe
+    end
+
+    def to_s
+      self
+    end
+
+    def to_param
+      to_str
+    end
+
+    def encode_with(coder)
+      coder.represent_scalar nil, to_str
+    end
+
+    UNSAFE_STRING_METHODS.each do |unsafe_method|
+      if 'String'.respond_to?(unsafe_method)
+        class_eval <<-EOT, __FILE__, __LINE__ + 1
+          def #{unsafe_method}(*args, &block)       # def capitalize(*args, &block)
+            to_str.#{unsafe_method}(*args, &block)  #   to_str.capitalize(*args, &block)
+          end                                       # end
+
+          def #{unsafe_method}!(*args)              # def capitalize!(*args)
+            @html_safe = false                      #   @html_safe = false
+            super                                   #   super
+          end                                       # end
+        EOT
+      end
+    end
   end
 end
 
